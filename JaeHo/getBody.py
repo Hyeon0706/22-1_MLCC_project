08b023772a265c431e54b941489cbdd05f565afe
yoojaeho1 <meqdz9828@gmail.com>
2022-05-29 21:31:07 +0900

    전극/바디 부분 검출해서 자르기. (바디 이미지 크기 조절 필요)

--- JaeHo/getBody.py
+++ JaeHo/getBody.py
@@ -1,12 +1,17 @@
 import cv2
-import numpy as np
+
 
 def get_body(image_origin):
-    margin = 10  # 원하는 margin
+    margin = -10  # 원하는 margin
     image_copy = image_origin.copy()
-    imgray = cv2.cvtColor(image_copy, cv2.COLOR_BGR2GRAY)
-    ret, thr = cv2.threshold(imgray, 60, 255, cv2.THRESH_TOZERO)
-    contours, _ = cv2.findContours(thr, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret, thr = cv2.threshold(imgray, 127, 255, cv2.THRESH_TOZERO_INV)
+    _, binary = cv2.threshold(thr,50,255,cv2.THRESH_BINARY)
+    se = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
+    ope = cv2.morphologyEx(binary, cv2.MORPH_OPEN, se)
+    contours, _ = cv2.findContours(ope, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    contours, _ = cv2.findContours(ope, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     origin_height, origin_width = image_copy.shape[:2] 
     crop_images = []  # 자른 이미지를 하나씩 추가해서 저장할 리스트
 
@@ -21,17 +26,18 @@ def get_body(image_origin):
             crop_col_2 = (x + width + margin) if (x + width + margin) < origin_width else x + width
 
            
-            crop = image_copy[crop_row_1+20: crop_row_2-10, crop_col_1+55: crop_col_2-55]  # 이미지를 잘라낸다.
+            crop = image_copy[crop_row_1: crop_row_2, crop_col_1: crop_col_2]  # 이미지를 잘라낸다.
             crop_images.append(crop)  # 잘라낸 이미지들을 하나씩 리스트에 담는다.
 
     
     return crop_images[0]
 
 
+
 #테스트 용.
-#img = cv2.imread('001-1.tif')
+img = cv2.imread('C:\/finalProject\JaeHo\/160-3.tif')
 
-#cv2.imshow('image1', get_body(img))
+cv2.imshow('image1', get_body(img))
 
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
+cv2.waitKey(0)
+cv2.destroyAllWindows()
--- JaeHo/test.py
+++ JaeHo/test.py
@@ -1,9 +1,29 @@
 import cv2
-import load
 
-img = cv2.imread('001-1.tif')
 
-cv2.imshow('right', load.sliceEle1(img))
-cv2.imshow('left', load.sliceEle2(img))
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+def empty(pos):
+    #     print(pos)
+    pass
+
+img = cv2.imread('001-1.tif',cv2.IMREAD_GRAYSCALE)
+
+name = 'Trackbar'
+cv2.namedWindow(name)
+
+cv2.createTrackbar('threshold',name,127,255,empty) # bar 이름, 창의 이름, 초기값, 최대값, 이벤트 처리
+
+while 1:
+    thresh = cv2.getTrackbarPos('threshold',name) # bar 이름, 창의 이름
+    ret, binary_toz = cv2.threshold(img,thresh,255,cv2.THRESH_TOZERO_INV) # tozero_inv 로 전극 부분 검은색으로 만든 다음
+    _, binary = cv2.threshold(binary_toz,50,255,cv2.THRESH_BINARY) # 위 이미지를 이진화
+    se = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5)) # 이진화만 하면 테두리가 그대로 남아있어서
+    ope = cv2.morphologyEx(binary, cv2.MORPH_OPEN, se) # 열림 연산을 통해 노이즈 제거
+    if not ret:
+        break
+        
+    cv2.imshow(name,ope)
+    if cv2.waitKey(1) == ord('q'):
+        break
+    
+
+
